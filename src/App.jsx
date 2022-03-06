3dfba8b5c3ba56523af9b38bf78b579acdf6f1f7
throuz <64568129+throuz@users.noreply.github.com>
2022-03-06 12:58:34 +0800

    Complete Search UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { Stack, Box, Divider } from "@mui/material";
+import Search from "./components/Search";
 
 const App = () => {
   return (
-    <div className="App">
-      <Stack divider={<Divider />} spacing={2} sx={{ p: 2 }}>
-        <Box sx={{ typography: "h6" }}>Today's Weather</Box>
-        <Box sx={{ typography: "h6" }}>Today's Weather</Box>
-      </Stack>
-    </div>
+    <Stack className="App" divider={<Divider />} spacing={2} sx={{ p: 2 }}>
+      <Box sx={{ typography: "h6" }}>Today's Weather</Box>
+      <Search />
+    </Stack>
   );
 };
 
